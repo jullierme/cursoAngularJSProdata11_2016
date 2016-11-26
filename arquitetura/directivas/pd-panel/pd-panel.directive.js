08ada1420237981eff5c02f7a22d00d835d2e838
Jullierme Barros <julliermebarros@gmail.com>
2016-11-26 14:05:47 -0200

    aula diretivas parte 2

---
+++ arquitetura/directivas/pd-panel/pd-panel.directive.js
@@ -0,0 +1,28 @@
+(function () {
+    'use strict';
+
+    angular.module('pdAppArquitetura')
+        .directive('pdPanel', pdPanel);
+
+    function pdPanel() {
+        return {
+            restrict: 'E',
+            templateUrl: 'arquitetura/directivas/pd-panel/pd-panel.html',
+            transclude: {
+                header: '?pdPanelHeader',
+                body: '?pdPanelBody',
+                footer: '?pdPanelFooter'
+            },
+            scope: {
+                classPanel:'@',
+                exibirFooter:'=',
+                exibirHeader:'='
+            },
+            link: link
+        };
+
+        function link(scope, element, attrs){
+            scope.classPanel = scope.classPanel || 'panel-default';
+        }
+    }
+})();
